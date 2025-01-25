5983e555f02892dd7d15de46863a9c52667cf48a
Gilang <19211020@bsi.ac.id>
2025-01-25 21:23:39 +0700

    feat(category): define new dao for update,getLastPosition,and updatePosition

--- core/database/src/main/java/com/keep/database/dao/CategoryDao.kt
+++ core/database/src/main/java/com/keep/database/dao/CategoryDao.kt
@@ -5,16 +5,26 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import com.keep.database.model.CategoryEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface CategoryDao {
-    @Query("SELECT * FROM category")
+    @Query("SELECT * FROM category ORDER BY position ASC")
     fun getCategoryEntity(): Flow<List<CategoryEntity>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertCategory(categoryEntities: CategoryEntity)
+
+    @Update
+    suspend fun updateCategory(categoryEntities: CategoryEntity)
+
+    @Query("SELECT MAX(position) FROM category")
+    suspend fun getLastPosition() : Int?
+
+    @Query("UPDATE category SET position = :position WHERE categoryId = :id")
+    suspend fun updatePosition(id: Int, position: Int)
 
     @Delete
     suspend fun deleteCategory(categoryEntities: CategoryEntity)
