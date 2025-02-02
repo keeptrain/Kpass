67529b2ac34dd09333ffe509deab8890cf36708f
Gilang <19211020@bsi.ac.id>
2025-02-02 21:40:02 +0700

    feat(dashboard): add 4 cardview with gridlayout 2x2 in fragment_dashboard.xml

--- app/src/main/java/com/keep/password/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/keep/password/ui/dashboard/DashboardFragment.kt
@@ -4,56 +4,55 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.SeekBar
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
+import com.keep.password.MainActivity
 import com.keep.password.databinding.FragmentDashboardBinding
-import dagger.hilt.android.scopes.FragmentScoped
+import dagger.hilt.android.AndroidEntryPoint
 
-@FragmentScoped
+@AndroidEntryPoint
 class DashboardFragment : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private val dashboardViewModel : DashboardViewModel by viewModels()
+
+    private val mainActivity by lazy {
+        requireActivity() as MainActivity
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this)[DashboardViewModel::class.java]
-
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-
-        binding.seekbarLength.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-
-            }
-
-            override fun onStartTrackingTouch(seekBar: SeekBar?) {
-                TODO("Not yet implemented")
-            }
-
-            override fun onStopTrackingTouch(seekBar: SeekBar?) {
-                TODO("Not yet implemented")
-            }
-        })
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupGridCardView()
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun setupActionBar() {
+        val toolbar = binding.dashboardToolbar
+        mainActivity.setSupportActionBar(toolbar)
+    }
+
+    private fun setupGridCardView() {
+        binding.cardview1.setOnClickListener {
+//            mainActivity.goSettingsFragment(findNavController())
+        }
     }
 }
