c1a04a6e3455ca6ed421d45d714ff6bdea527cbb
Gilang <107345932+keeptrain@users.noreply.github.com>
2025-02-17 18:17:20 +0700

    feat(core/common): add View extension functions (hide,show,invisible)

---
+++ core/common/src/main/java/com/keep/common/extension/ViewExt.kt
@@ -0,0 +1,15 @@
+package com.keep.common.extension
+
+import android.view.View
+
+fun View.hide() {
+    this.visibility = View.GONE
+}
+
+fun View.show() {
+    this.visibility = View.VISIBLE
+}
+
+fun View.invisible() {
+    this.visibility = View.INVISIBLE
+}
