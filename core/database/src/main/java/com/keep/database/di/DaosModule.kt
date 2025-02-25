5e6e94d84fd68388d978543c6e82cdb308a931e2
Gilang <19211020@bsi.ac.id>
2025-02-25 17:24:11 +0700

    feat(core/database): add providesEntryDao method in DaosModule

--- core/database/src/main/java/com/keep/database/di/DaosModule.kt
+++ core/database/src/main/java/com/keep/database/di/DaosModule.kt
@@ -2,15 +2,20 @@ package com.keep.database.di
 
 import com.keep.database.KpsDatabase
 import com.keep.database.dao.CategoryDao
+import com.keep.database.dao.EntryDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
-
 @Module
 @InstallIn(SingletonComponent::class)
 internal object DaosModule {
+
+    @Provides
+    fun providesEntryDao(
+        database: KpsDatabase
+    ): EntryDao = database.entryDao()
 
     @Provides
     fun providesCategoryDao(
