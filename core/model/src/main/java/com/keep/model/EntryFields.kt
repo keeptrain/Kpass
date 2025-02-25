d8a0635623b5cbb60b62eb442e8e9ac9c59132e7
Gilang <19211020@bsi.ac.id>
2025-02-25 17:17:20 +0700

    refactor(core/model): change propertyName,propertyValue to fieldName,fieldValue at EntryFields

--- core/model/src/main/java/com/keep/model/EntryFields.kt
+++ core/model/src/main/java/com/keep/model/EntryFields.kt
@@ -2,6 +2,6 @@ package com.keep.model
 
 data class EntryFields(
     val entryId: Int,
-    val propertyName: String,
-    val propertyValue: String,
+    val fieldName: String,
+    val fieldValue: String,
 )
