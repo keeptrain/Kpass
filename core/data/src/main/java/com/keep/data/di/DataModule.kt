206552fbe4e4db77d87ac795381a56b3baeb0dcb
Gilang <19211020@bsi.ac.id>
2025-02-26 20:06:51 +0700

    feat(core/data): define binds methode in DataModule for EntryRepository

--- core/data/src/main/java/com/keep/data/di/DataModule.kt
+++ core/data/src/main/java/com/keep/data/di/DataModule.kt
@@ -1,7 +1,9 @@
 package com.keep.data.di
 
 import com.keep.data.repository.CategoryRepositoryImpl
+import com.keep.data.repository.EntryRepositoryImpl
 import com.keep.domain.repository.CategoryRepository
+import com.keep.domain.repository.EntryRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -10,6 +12,11 @@ import dagger.hilt.components.SingletonComponent
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class DataModule {
+
+    @Binds
+    internal abstract fun bindsEntryRepository(
+        entryRepositoryImpl: EntryRepositoryImpl
+    ): EntryRepository
 
     @Binds
     internal abstract fun bindsCategoryRepository(
