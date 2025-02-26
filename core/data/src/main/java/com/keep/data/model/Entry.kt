cb5ccd1117e1778de88626f562631e833b53146f
Gilang <19211020@bsi.ac.id>
2025-02-26 20:03:50 +0700

    refactor(core/database): change package import for EntryEntity

--- core/data/src/main/java/com/keep/data/model/Entry.kt
+++ core/data/src/main/java/com/keep/data/model/Entry.kt
@@ -1,6 +1,6 @@
 package com.keep.data.model
 
-import com.keep.database.model.EntryEntity
+import com.keep.database.model.entry.EntryEntity
 import com.keep.model.Entry
 
 fun Entry.toEntity() = EntryEntity(
