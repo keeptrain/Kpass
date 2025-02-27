7d7d2ebe8b47f28efbcd414e3850256a5a9152a1
Gilang <19211020@bsi.ac.id>
2025-02-27 15:56:40 +0700

    refactor(core/data): adjust relevant methode for EntryRepositoryImpl

--- core/data/src/main/java/com/keep/data/repository/EntryRepositoryImpl.kt
+++ core/data/src/main/java/com/keep/data/repository/EntryRepositoryImpl.kt
@@ -1,31 +1,36 @@
 package com.keep.data.repository
 
 import com.keep.data.model.toEntity
+import com.keep.database.BinDispatcher
+import com.keep.database.Dispatcher
 import com.keep.database.dao.EntryDao
-import com.keep.database.model.toExternalModel
+import com.keep.database.model.entry.toExternalModel
 import com.keep.domain.repository.EntryRepository
 import com.keep.model.Entry
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class EntryRepositoryImpl @Inject constructor(
+    @Dispatcher(BinDispatcher.IO) private val ioDispatcher: CoroutineDispatcher,
     private val entryDao: EntryDao
 ) : EntryRepository {
 
-    override fun getEntry(): List<Entry> {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun getCategoryList(): Flow<List<Entry>> {
-        return entryDao.getCategoryList().map {
+    override fun getEntry(): Flow<List<Entry>> {
+        return entryDao.getEntryEntity().map {
             it.map {
                 it.toExternalModel()
             }
-        }
+        }.flowOn(ioDispatcher)
     }
 
-    override suspend fun insertEntry(entry: Entry) {
-        return entryDao.insertEntry(entry.toEntity())
+    override fun upsertEntry(entry: Entry) {
+        CoroutineScope(ioDispatcher).launch {
+            entryDao.upsertEntry(entry.toEntity())
+        }
     }
 }
