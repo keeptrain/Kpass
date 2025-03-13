58cae33b38ccca8e7f1b4aae0c0c4fef4a329fac
Gilang <19211020@bsi.ac.id>
2025-03-13 19:54:43 +0700

    refactor(core/domain): rename value EntryFields from model to EntryField

--- core/domain/src/main/java/com/keep/domain/repository/EntryFieldRepository.kt
+++ core/domain/src/main/java/com/keep/domain/repository/EntryFieldRepository.kt
@@ -1,9 +1,9 @@
 package com.keep.domain.repository
 
-import com.keep.model.EntryFields
+import com.keep.model.EntryField
 import kotlinx.coroutines.flow.Flow
 
 interface EntryFieldRepository {
-    fun getEntryField(): Flow<List<EntryFields>>
-    fun upsertEntryField(entryFields: EntryFields)
+    fun getEntryField(): Flow<List<EntryField>>
+    fun upsertEntryField(entryField: EntryField)
 }
